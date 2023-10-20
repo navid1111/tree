237813e64c53b471328a2e3d680e8460391fbd76
navid-kamal <navidkamal@iut-dhaka.edu>
2023-10-20 23:01:15 +0600

    rest of the code

--- verticalview.cpp
+++ verticalview.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define fr first
-#define sc second
+
 struct node
 {
 	int data;
@@ -10,9 +9,56 @@ struct node
 
 };
 vector<vector<int>>findVertical(node *root) {
-	map<int, map<int , multiset<int>>>nodes;
-	queue<pair<node*, pair<int, int>>>todo;
+	map < int, map < int, multiset < int >>> nodes;
+	queue < pair < node * , pair < int, int >>> todo;
+	todo.push({
+		root, {
+			0,
+			0
+		}
+	});
+	while (!todo.empty()) {
+		auto p = todo.front();
+		todo.pop();
+		node * temp = p.first;
+
+		int x = p.second.first, y = p.second.second;
+		nodes[x][y].insert(temp->data);
+		if (temp->left) {
+			todo.push({
+				temp->left,
+				{
+					x - 1,
+					y + 1
+				}
+
+			});
+			if (temp->right) {
+				todo.push({
+					temp->right,
+					{
+						x + 1,
+						y + 1
+					}
+
+				});
+
+			}
+
+		}
+	}
+	vector<vector<int>>ans;
+	for (auto p : nodes) {
+		vector<int>col;
+		for (auto q : p.second) {
+			col.insert(col.end(), q.second.begin(), q.second.end());
+		}
+		ans.push_back(col);
+	}
+	return ans;
+
 }
+
 struct node * newNode(int data) {
 	struct node * node = (struct node * ) malloc(sizeof(struct node));
 	node -> data = data;
@@ -33,5 +79,15 @@ int main() {
 	root -> right = newNode(3);
 	root -> right -> left = newNode(9);
 	root -> right -> right = newNode(10);
+	vector < vector < int > > verticalTraversal;
+	verticalTraversal = findVertical(root);
+
+	cout << "The Vertical Traversal is : " << endl;
+	for (auto vertical : verticalTraversal) {
+		for (auto nodeVal : vertical) {
+			cout << nodeVal << " ";
+		}
+		cout << endl;
+	}
 
 }
