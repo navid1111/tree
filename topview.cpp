5024d84ceebd82454e6eba94f41f13214820af91
navid-kamal <navidkamal@iut-dhaka.edu>
2023-10-21 06:58:55 +0600

    topView function

--- topview.cpp
+++ topview.cpp
@@ -8,6 +8,24 @@ struct node
 	struct node *left , *right;
 
 };
+void leftView(node *root, vector<int>&v) {
+	if (root == NULL)return;
+
+	leftView(root->left, v);
+	v.push_back(root->data);
+}
+void rightView(node *root, vector<int>&v) {
+	if (root == NULL)return;
+	v.push_back(root->data);
+	rightView(root->right, v);
+
+}
+void topView(node *root, vector<int>&v) {
+
+	leftView(root, v);
+	rightView(root->right, v);
+
+}
 
 struct node * newNode(int data) {
 	struct node * node = (struct node * ) malloc(sizeof(struct node));
@@ -20,16 +38,19 @@ struct node * newNode(int data) {
 
 int main() {
 
-	struct node * root = newNode(1);
-	root -> left = newNode(2);
-	root -> left -> left = newNode(4);
-	root -> left -> right = newNode(10);
-	root -> left -> left -> right = newNode(5);
-	root -> left -> left -> right -> right = newNode(6);
-	root -> right = newNode(3);
-	root -> right -> left = newNode(9);
-	root -> right -> right = newNode(10);
-	vector < vector < int > > verticalTraversal;
+	struct node * root = newNode(10);
+	root -> left = newNode(20);
+	root -> left -> left = newNode(40);
+	root -> right = newNode(30);
+	root -> left -> right  = newNode(60);
+	root -> right->right = newNode(100);
+	root -> right -> left = newNode(90);
+	vector < int > topview;
+	topView(root, topview);
+	for (auto it : topview) {
+		cout << it << " ";
+	}
+
 
 
 }
